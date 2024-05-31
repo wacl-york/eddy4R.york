1bb610c7d47175569e9e133e8abbf622aed68c5e
willdrysdale <wsd500@york.ac.uk>
2024-05-31 14:38:45 +0100

    initial commit

---
+++ R/docker_test.R
@@ -0,0 +1,12 @@
+#' docker test
+#'
+#' function to test that the package has correctly been added to the docker image
+#'
+#' @export
+#'
+
+docker_test = function(){
+
+  print("Hello Docker")
+
+}
