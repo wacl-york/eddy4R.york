4ea5fdc1d78e3b3fa4ad561890fe0cf8e70f8574
willdrysdale <wsd500@york.ac.uk>
2024-06-18 15:41:24 +0100

    update def.para.tmp to also change lag config when skipping scalar

--- R/def.para.tmp.R
+++ R/def.para.tmp.R
@@ -14,8 +14,13 @@ def.para.tmp = function(paraMain, skip_scalar){
 
   paraCall = paraMain$call
 
+  skipRtio = paraMain$speciesRatioName[which(paraMain$species %in% c(skip_scalar))]
+  skipCcvPos = which(paraMain$cross_correlation_vars %in% skipRtio)
+
   # edit the original call to only the species we have
   paraCall$species = paraCall$species[paraCall$species != skip_scalar]
+  paraCall$absolute_lag = eval(paraCall$absolute_lag)[-skipCcvPos]
+  paraCall$lag_boundary = eval(paraCall$lag_boundary)[-skipCcvPos]
 
   # If there are no species left, make it NULL
   if(length(paraCall$species) == 0){
