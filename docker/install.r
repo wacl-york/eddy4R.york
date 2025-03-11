e3bced7b1d0711779209dcad226b7649898c919b
willdrysdale <wsd500@york.ac.uk>
2025-03-11 16:24:13 +0000

    change wrap.rot for R CMD + minor issues
    - Swap class(x) != "y" for !"y" %in% class(x)
    - docs updates

--- docker/install.r
+++ docker/install.r
@@ -1,5 +1,4 @@
 devtools::install("/home/york/eddy4r.york")
-writeLines("_R_CHECK_SYSTEM_CLOCK_=0", "/home/rstudio/.Renviron")
 install.packages("duckdb",repos = "https://p3m.dev/cran/__linux__/jammy/2024-06-14")
 
 options(digits.secs=3)
