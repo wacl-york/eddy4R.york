c0f39a981d38d3b81e6dd00bc63726254fc5fba1
willdrysdale <wsd500@york.ac.uk>
2025-12-02 11:31:25 +0000

    stop def.lod() looping over NULL vars
     - the loop in def.lod() was structured as for(var in names(measCol)) but names(measCol) == NULL. Restructure to for(var in meanCol)
    
    - Also, was lagging against veloXaxs as refe, changes to veloZaxs
    - Finally updated the if logic to be more readable.

--- eddy4r.york/R/def.lod.R
+++ eddy4r.york/R/def.lod.R
@@ -1,71 +1,62 @@
 #' Flux Limit of Detection
-#' 
+#'
 #' Langford et al., 2015 Eddy-covariance data with low signal-to-noise ratio method
 #' of flux Limit of detection. Based on original implementation by Adam Vaughan
-#' 
+#'
 #' @param REYN eddy4R REYN list - requires mean and diff
 #' @param measCol columns to calculate LOD for
 #' @param freq measurement frequency
 #' @param refCol column to lag against, default veloZaxs
-#' 
+#'
 #' @export
 
 def.lod = function(REYN,
                    measCol,
                    freq,
-                   refCol = "veloXaxs"){
-  
+                   refCol = "veloZaxs"){
+
   # Satisfy CMD Check
   lag = conf = NULL
-  
-  
-  lods = vector(mode = "numeric", length = length(measCol)+1) %>% 
+
+
+  lods = vector(mode = "numeric", length = length(measCol)+1) %>%
     stats::setNames(c(measCol, "conf"))
-  
-  for(var in names(measCol)){
-    
+
+  for(var in measCol){
+
     #Langford et al., (2015), Eddy-covariance data with low signal-to-noise ratio, page 4201
     spec <- stats::ccf(x = REYN$diff[[refCol]],
                        y = REYN$diff[[var]],
                        type="covariance",
                        lag.max=180*freq,
                        plot=F)
-    
+
     specDf <- data.frame(lag=spec$lag,
-                         acf=spec$acf) %>% 
+                         acf=spec$acf) %>%
       dplyr::filter(lag > 150*freq | lag < -150*freq)
-    
+
     er <- stats::sd(specDf$acf,na.rm=T)
- 
-    if(var %in% c("rtioMoleDryH2o", "tempAir")){
-      # Latent Heat
-      if(var == "rtioMoleDryH2o"){
-        lods[var] <- REYN$mean$densMoleAirDry * er
-      }
-      # Sensible Heat
-      if(var == "tempAir"){
-        lods[var] <- er
-        
-      }
+
+    # This is the same as "conv" that is passed to def.flux.sclr. This is nothing for sens heat, but is the desity of air for latent heat and gas scalars. Definied in listGasSclr as densMoleAirDry
+    if(var == "tempAir"){
+      lods[var] <- er
+    }else{
+      lods[var] <- REYN$mean$densMoleAirDry * er
     }
-    
-    if(length(measCol) > 2){
-      lods[var] =  REYN$mean$densMoleAirDry * er
-    }
-    
+
   }
-  
+
   output = as.data.frame(
     t(cbind(
       lods*1.96,
       lods*3
     )))
-  
+
   output$conf = c(95, 99)
-  
+
   #
-  
+
   tidyr::pivot_longer(output, -conf)
-  
+
 }
 
